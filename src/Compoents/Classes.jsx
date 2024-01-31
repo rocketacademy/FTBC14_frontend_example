802c8c2af606fba0f96e9d0850a357b9e6d56852
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2024-01-31 19:50:36 +0800

    adding additional users in classes

--- src/Compoents/Classes.jsx
+++ src/Compoents/Classes.jsx
@@ -3,17 +3,24 @@ import { useNavigate } from "react-router-dom";
 
 import Select from "react-select";
 
-const options = [
-  { value: 1, label: "Bob" },
-  { value: 2, label: "Jessica" },
-  { value: 3, label: "Joey" },
-];
-
 export default function Classes(props) {
   const navigate = useNavigate();
 
   const [classes, setClasses] = useState([]);
   const [selectedOption, setSelectedOption] = useState(1);
+  const [users, setUsers] = useState([]);
+
+  let options = [
+    { value: 1, label: "Bob" },
+    { value: 2, label: "Jessica" },
+    { value: 3, label: "Joey" },
+  ];
+
+  if (users.length > 0) {
+    options = users.map((user) => {
+      return { value: user.id, label: user.firstName };
+    });
+  }
 
   useEffect(() => {
     let getData = async () => {
@@ -24,16 +31,29 @@ export default function Classes(props) {
       setClasses(data.data);
     };
     getData();
+
+    let getUsers = async () => {
+      let data = await props.axios.get(
+        `${import.meta.env.VITE_SOME_BACKEND_URL}/students`
+      );
+      console.log(data.data);
+      console.log(data.data);
+      setUsers(data.data);
+    };
+
+    getUsers();
   }, []);
 
   const handleAssign = async (classId) => {
     console.log(classId);
+    let token = localStorage.getItem("jwtAccessToken");
     await props.axios.post(
       `${import.meta.env.VITE_SOME_BACKEND_URL}/students/classes`,
       {
         userId: selectedOption.value,
         classId: classId,
-      }
+      },
+      { headers: { Authorization: `Bearer ${token}` } }
     );
 
     navigate("/usersClasses");
